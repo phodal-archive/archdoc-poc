00717c2a2b5260420dfdc91c6f8b6d831ff2c684
Phodal Huang <h@phodal.com>
2022-05-09 21:17:47 +0800

    test: fix libs issues

--- src/jvmTest/kotlin/org/archguard/archdoc/ReplTest.kt
+++ src/jvmTest/kotlin/org/archguard/archdoc/ReplTest.kt
@@ -11,25 +11,26 @@ import org.junit.jupiter.api.Test
 import kotlin.script.experimental.jvm.util.scriptCompilationClasspathFromContext
 
 internal class DslTest {
-    val classpath = scriptCompilationClasspathFromContext(
-        "kotlin-jupyter-lib",
-        "kotlin-jupyter-api",
-        "kotlin-jupyter-shared-compiler",
-
-        // kotlin libs
-        "kotlin-stdlib",
-        "kotlin-reflect",
-        "kotlin-script-runtime",
-        classLoader = DependsOn::class.java.classLoader
-    )
-    private val resolutionInfoProvider = EmptyResolutionInfoProvider
-    private val repl = ReplForJupyterImpl(resolutionInfoProvider, classpath)
-
-    private fun eval(code: Code, displayHandler: DisplayHandler? = null, jupyterId: Int = -1, storeHistory: Boolean = true) =
-        repl.eval(EvalRequestData(code, displayHandler, jupyterId, storeHistory))
-
     @Test
-    internal fun simple_layered() {
+    internal fun simple_eval() {
+        val classpath = scriptCompilationClasspathFromContext(
+            "kotlin-jupyter-lib",
+            "kotlin-jupyter-api",
+            "kotlin-jupyter-shared-compiler",
+            // kotlin libs
+            "kotlin-stdlib",
+            "kotlin-reflect",
+            "kotlin-script-runtime",
+            classLoader = DependsOn::class.java.classLoader
+        )
+        val resolutionInfoProvider = EmptyResolutionInfoProvider
+        val repl = ReplForJupyterImpl(resolutionInfoProvider, classpath)
+
+//        val embeddedClasspath: List<File> = System.getProperty("java.class.path").split(File.pathSeparator).map(::File)
+
+        fun eval(code: Code, displayHandler: DisplayHandler? = null, jupyterId: Int = -1, storeHistory: Boolean = true) =
+            repl.eval(EvalRequestData(code, displayHandler, jupyterId, storeHistory))
+
         eval("val x = 3")
         val res = eval("x*2")
         res.resultValue shouldBe 6
